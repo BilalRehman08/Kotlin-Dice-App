3f7e705ea3c226c658c3f23cba14490b29cb3707
BilalRehman08 <bilalrehman080808@gmail.com>
2022-09-05 16:15:26 +0500

    dice app with image

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -10,7 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
@@ -22,9 +22,17 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val obj = Dice(6)
         val diceValue = obj.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceValue.toString()
-
+        val image: ImageView = findViewById(R.id.imageView)
+        val diceImage = when(diceValue){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        image.setImageResource(diceImage)
+        image.contentDescription = diceValue.toString()
     }
 }
 
